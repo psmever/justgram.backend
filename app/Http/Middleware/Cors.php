4dbfcbb2b8cd61fc5f03a4c9b498e4c157a0a795
psmever <psmever@gmail.com>
2020-01-07 20:51:17 +0900

    client type add.

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
 	    return $next($request)
 		    ->header('Access-Control-Allow-Origin', '*')
 		    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-		    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
+		    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN, Request-Client-Type');
     }
 
 }

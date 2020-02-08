1e1a44ea3a2ae6df47febfe7038883dcd51c981a
psmever <psmever@gmail.com>
2020-02-08 19:20:14 +0900

    공통 코드 모델 추가.

---
+++ app/Models/JustGram/CodesMaster.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\JustGram;
+
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Model;
+
+class CodesMaster extends BaseModel
+{
+    protected $table = "tbl_codes_master";
+}

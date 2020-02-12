a391bcb11e56af5847808546c2fd3f4b10e65695
psmever <psmever@gmail.com>
2020-02-12 21:49:50 +0900

    cafe24 db migrate:fresh 에러.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //TODO: Passport::ignoreMigrations()
-	    Passport::ignoreMigrations();
+        Passport::ignoreMigrations();
+
     }
 
     /**
@@ -25,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }

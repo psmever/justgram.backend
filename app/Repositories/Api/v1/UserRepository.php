72e4a996d25af06e0320caf917cda4f327391d70
psmever <psmever@gmail.com>
2020-03-12 00:32:39 +0900

    사용자 체크 및 사용자 프로필 정보 전잘 추가.

--- app/Repositories/Api/v1/UserRepository.php
+++ app/Repositories/Api/v1/UserRepository.php
@@ -11,10 +11,65 @@ use App\Traits\Model\UserTrait;
 
 class UserRepository implements UserRepositoryInterface
 {
-	use UserTrait;
+    use UserTrait;
 
-	public function start()
-	{
+    private $UserUUID;
+    private $UserID;
 
-	}
+	public function start() {
+
+    }
+
+    /**
+     * 사용자 체크.
+     *
+     * @param string $user_name
+     * @return array
+     */
+    public function checkUser(string $user_name = NULL) : array {
+
+        if(empty($user_name)) {
+            return [
+                "state" => false,
+                "message" => __('messages.exits_value.user'),
+            ];
+        }
+
+        $checkResult = self::checkExitsUserName($user_name);
+
+        if ($checkResult['state'] == false) {
+            return [
+                "state" => false,
+                "message" => __('messages.exits.user'),
+            ];
+        }
+        //TODO: 사용자 상태 체크를 어떻게 할지?
+
+        $this->UserUUID = $checkResult['data']['user_uuid'];
+        $this->UserID = $checkResult['data']['user_id'];
+
+        return [
+            "state" => true,
+        ];
+    }
+
+    /**
+     * 사용자 프로필 정보 전달.
+     *
+     * @return void
+     */
+    public function getUserProfileData() {
+        $taskResult = self::secondGetUserProfileData($this->UserID);
+        if($taskResult['state']) {
+            return [
+                'state' => true,
+                'data' => $taskResult['data']
+            ];
+        } else {
+            return [
+                'state' => true,
+                'message' => __('messages.exits.data'),
+            ];
+        }
+    }
 }

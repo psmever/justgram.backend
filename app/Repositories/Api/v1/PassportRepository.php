2dce43fe2c2fd41310f254449e2f53db37ea465a
psmever <psmever@gmail.com>
2020-03-16 01:11:40 +0900

    토큰 발행, 토큰 리프레쉬 수정.

--- app/Repositories/Api/v1/PassportRepository.php
+++ app/Repositories/Api/v1/PassportRepository.php
@@ -14,10 +14,15 @@ use App\Mail\v1\EmailMaster;
 use Illuminate\Support\Facades\Route as FacadesRoute;
 
 use App\Traits\Model\UserTrait;
+use App\Traits\OauthTrait;
 
 class PassportRepository implements PassportRepositoryInterface
 {
-    use UserTrait;
+    use UserTrait, OauthTrait {
+        OauthTrait::getNewToken as getNewTokenTrait;
+        OauthTrait::getRefreshToken as getRefreshTokenTrait;
+        UserTrait::getUserProfileImageUrl as getUserProfileImageUrlTrait;
+    }
 
 	public function start()
 	{
@@ -38,8 +43,7 @@ class PassportRepository implements PassportRepositoryInterface
             'username' => 'required',
         ]);
 
-		if( $validator->fails() )
-		{
+		if( $validator->fails() ) {
             $errorMessage = $validator->getMessageBag()->all();
 			return [
 				'state' => false,
@@ -55,52 +59,45 @@ class PassportRepository implements PassportRepositoryInterface
 			'user_name' => preg_replace("/[^a-z0-9]/i", "", strtolower($request->input('username'))), // 소문자, 및 숫자만.
 			'email' => $request->input('email'),
 			'password' => bcrypt($request->input('password')),
-		]);
+        ]);
 
-		if($createResult->wasRecentlyCreated)
-		{
-			$auth_code = Str::random(80);
-
-			EmailAuth::create([
-				'user_uuid' => $newUserUUID,
-				'auth_code' => $auth_code,
-			]);
-
-			// TODO: 사용자 등록시 인증 메일
-			$emailObject = new \stdClass();
-			$emailObject->category = "user_email_auth";
-			$emailObject->receiverName = $request->input('name');
-			$emailObject->receiver = $request->input('email');
-			$emailObject->auth_code = $auth_code;
-			$emailObject->auth_url = url('/front/v1/auth/email_auth?code='.$auth_code);
-
-			Mail::to("psmever@gmail.com")->send(new EmailMaster($emailObject));
-
-			if(Mail::failures())
-			{
-				return [
-					'state' => false,
-					'message' => __('messages.default.error')
-				];
-			}
-			else
-			{
-				return [
-					'state' => true,
-					'data' => [
-						'uuid' => $newUserUUID
-					]
-				];
-			}
-
-		}
-		else
-		{
-			return [
+        if(!$createResult->wasRecentlyCreated) {
+            return [
 				'state' => false,
 				'message' => __('messages.default.error')
 			];
-		}
+        }
+
+        $auth_code = Str::random(80);
+
+        EmailAuth::create([
+            'user_uuid' => $newUserUUID,
+            'auth_code' => $auth_code,
+        ]);
+
+        // TODO: 사용자 등록시 인증 메일
+        $emailObject = new \stdClass();
+        $emailObject->category = "user_email_auth";
+        $emailObject->receiverName = $request->input('name');
+        $emailObject->receiver = $request->input('email');
+        $emailObject->auth_code = $auth_code;
+        $emailObject->auth_url = url('/front/v1/auth/email_auth?code='.$auth_code);
+
+        Mail::to("psmever@gmail.com")->send(new EmailMaster($emailObject));
+
+        if(Mail::failures()) {
+            return [
+                'state' => false,
+                'message' => __('messages.default.error')
+            ];
+        } else {
+            return [
+                'state' => true,
+                'data' => [
+                    'uuid' => $newUserUUID
+                ]
+            ];
+        }
 	}
 
 	/**
@@ -110,109 +107,81 @@ class PassportRepository implements PassportRepositoryInterface
 	 */
 	public function attemptLogin(Request $request) : array
 	{
-		if(Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
 
-            $user = Auth::user();
-
-            $user_name = $user['user_name'];
-			$user_state = $user['user_state'];
-            $user_active = $user['user_active'];
-            $profile_active = $user['profile_active'];
-            $profile_image = $user['profile_image'];
-
-			if($user_active != 'Y') // 사용자 상태 체크
-			{
-				return [
-					'state' => false,
-					'message' => __('auth.login.not_active_user')
-				];
-			}
-
-			if($user_state == 'A10000') // 사용자 대기 체크
-			{
-				return [
-					'state' => false,
-					'message' => __('auth.login.wait_user')
-				];
-			}
-
-            // request 를 생성해서 해야 하는데 그게 안됨.. 이유를 모르겠음.. 무한 뻉뻉이 돔?
-			$request->request->add([
-				'grant_type' => 'password',
-				'client_id' => env('PASSPORT_PASSWORD_GRANT_CLIENT_ID'),
-				'client_secret' => env('PASSPORT_PASSWORD_GRANT_CLIENT_SECRET'),
-				'username' => $request->input('email'),
-				'password' => $request->input('password'),
-				'scope' => '',
-			]);
-
-			$tokenRequest = $request->create(
-				url('/api/v1/oauth/token'),
-				'post'
-            );
-
-            if($tokenRequest) {
-                $data = json_decode(FacadesRoute::dispatch($tokenRequest)->getContent());
-                $data->user_name = $user_name;
-                $data->profile_active = $profile_active;
-                $data->profile_image_url = NULL;
-
-                if(!empty($profile_image)) {
-                    $profileImageResult = self::getUserProfileImageUrl($profile_image);
-                    if($profileImageResult['state'] == true) {
-                        $data->profile_image_url = $profileImageResult["data"]["secure_url"];
-                    }
-                }
-
-                return [
-                    'state' => true,
-                    'data' => $data
-                ];
-
-            } else {
-                return [
-                    'state' => false,
-                    'message' => __('auth.failed')
-                ];
-            }
-
-		} else {
-			return [
+        if(!Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            return [
 				'state' => false,
 				'message' => __('auth.failed')
 			];
-		}
+        }
+
+        $user = Auth::user();
+
+        $tokenResult = $this->getNewTokenTrait($request->input('email'), $request->input('password'));
+
+        $user_name = $user['user_name'];
+        $user_state = $user['user_state'];
+        $user_active = $user['user_active'];
+        $profile_active = $user['profile_active'];
+        $profile_image = $user['profile_image'];
+        $profile_image_url = NULL;
+
+        // 사용자 상태 체크
+        if($user_active != 'Y') {
+            return [
+                'state' => false,
+                'message' => __('auth.login.not_active_user')
+            ];
+        }
+
+        // 사용자 대기 체크
+        if($user_state == 'A10000') {
+            return [
+                'state' => false,
+                'message' => __('auth.login.wait_user')
+            ];
+        }
+
+        if(!empty($profile_image)) {
+            $profileImageResult = self::getUserProfileImageUrlTrait($profile_image);
+            if($profileImageResult['state'] == true) {
+                $profile_image_url = $profileImageResult["data"]["secure_url"];
+            }
+        }
+
+        $returnData = [
+            'token_type' => $tokenResult['token_type'],
+            'expires_in' => $tokenResult['expires_in'],
+            'access_token' => $tokenResult['access_token'],
+            'refresh_token' => $tokenResult['refresh_token'],
+            'user_name' => $user_name,
+            'profile_active' => $profile_active,
+            'profile_image_url' => $profile_image_url,
+        ];
+
+        return [
+            'state' => true,
+            'data' => $returnData
+        ];
 	}
 
 	public function attemptTokenRefresh(Request $request) : array
 	{
-		$UserData = Auth::user();
+        $UserData = Auth::user();
 
-		if($UserData)
-		{
-			$request->request->add([
-				'grant_type' => 'refresh_token',
-				'client_id' => env('PASSPORT_PASSWORD_GRANT_CLIENT_ID'),
-				'client_secret' => env('PASSPORT_PASSWORD_GRANT_CLIENT_SECRET'),
-				'refresh_token' => $request->input('refresh_token'),
-				'scope' => '',
-			]);
-
-			$tokenRequest = $request->create(
-				url('/api/v1/oauth/token'),
-				'post'
-			);
-
-			return [
-				'state' => true,
-				'data' => json_decode(FacadesRoute::dispatch($tokenRequest)->getContent())
+        if (!$UserData) {
+            return [
+				'state' => false,
+				'message' => __('auth.failed')
 			];
         }
 
-		return [
-			'state' => true
-		];
+        $taskResult = self::getRefreshTokenTrait($request->input('refresh_token'));
 
+        return [
+            'state' => true,
+            'data' => $taskResult
+        ];
 	}
 
 }

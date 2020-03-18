b206002f31d742cd4c565f1059c7bea10a990295
psmever <psmever@gmail.com>
2020-03-18 23:26:38 +0900

    passport 수정.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # /bin/zsh
 php artisan config:clear
 php artisan migrate:refresh --seed
-php artisan passport:install --force
+php artisan passport:install
 php artisan optimize
 composer dump-autoload

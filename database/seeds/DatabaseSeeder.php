5b8c61744322898f556b0316fb0a8c1c527246ed
psmever <psmever@gmail.com>
2020-03-18 23:28:46 +0900

    기본 사용자 seeder 추가.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
          $this->call(TblCodesMasterTableSeeder::class);
+         $this->call(TblUsersMasterSeeder::class);
     }
 }

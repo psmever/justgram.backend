674953508f1a517fb38356a42b5f662b66a7171f
psmever <psmever@gmail.com>
2020-11-22 01:34:42 +0900

    [UPDATE] rm cached.

--- build/lib/logger.js
+++ build/lib/logger.js
@@ -1,24 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = void 0;
-var winston = require("winston");
-var devTransport = new winston.transports.Console({
-    format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json(), winston.format.printf(function (info) {
-        return "\u001B[34m[" + info.timestamp + "]\u001B[39m " + info.level + ": " + info.message;
+const winston = require("winston");
+const devTransport = new winston.transports.Console({
+    format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), winston.format.json(), winston.format.printf((info) => {
+        return `\u001B[34m[${info.timestamp}]\u001b[39m ${info.level}: ${info.message}`;
     })),
 });
-var prodTransport = new winston.transports.Console({
-    format: winston.format.combine(winston.format.simple(), winston.format.printf(function (info) {
-        return "\u001B[39m " + info.level + ": " + info.message;
+const prodTransport = new winston.transports.Console({
+    format: winston.format.combine(winston.format.simple(), winston.format.printf((info) => {
+        return `\u001b[39m ${info.level}: ${info.message}`;
     })),
 });
-var transports = [devTransport];
+const transports = [devTransport];
 exports.logger = winston.createLogger({
     exitOnError: false,
-    transports: transports,
+    transports,
 });
-// Example logger function
-// logger.info(`Message info`);
-// logger.warn(`Message warn`);
-// logger.error(`Message error`);
 //# sourceMappingURL=logger.js.map

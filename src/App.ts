e79ce7c27f2f18a11490fb0890736ee88d99703e
psmever <psmever@gmail.com>
2020-11-22 02:07:09 +0900

    [ADD] jade, main page.

--- src/App.ts
+++ src/App.ts
@@ -1,19 +1,23 @@
 import * as express from 'express';
+import * as path from 'path';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 
 import TestRoute from './routes/TestRoute';
 import authRoute from './routes/v1/AuthRoute';
 
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 
 const mainRouter = (_req: express.Request, res: express.Response) => {
-    res.json({
-        message: 'Welcome To JustGram Backend Server'
-    });
+    res.render('index', { title: 'JustGram.Backend' });
 }
 
 const setupApp = (): express.Express => {
-    const app = express();
+
     app.use((req, res, next) => {
         // console.log(res)
         next();

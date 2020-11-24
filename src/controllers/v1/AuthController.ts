51ff8de6284c0ecdcd21d880fbc108acb0fc501b
psmever <psmever@gmail.com>
2020-11-24 22:05:21 +0900

    reset.

--- src/controllers/v1/AuthController.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-
-// export const postRegister = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-export const postRegister = (req: express.Request, res: express.Response) => {
-    res.json({
-        controller : 'AuthController'
-    });
-};
--- src/routes/TestRoute.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-import { getTest } from "src/controllers/TestController";
-
-const TestRoute = express.Router();
-
-// TestRoute.post('/admin/signup', verifyAuth, createAdmin);
-TestRoute.get("/test", getTest);
-
-export default TestRoute;

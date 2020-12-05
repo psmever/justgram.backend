9b5ffb4f07b1f560822589f1d1c2c53a700bea9c
psmever <psmever@gmail.com>
2020-12-06 00:18:11 +0900

    [Working] base.

---
+++ database/seeders/20201205115812-codes.js
@@ -0,0 +1,44 @@
+'use strict';
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
+
+console.log(config);
+console.log(config.use_env_variable);
+
+
+const codes = require('../seeditems/codes-seed');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+   var options = { raw: true };
+   await queryInterface.sequelize.query("SET FOREIGN_KEY_CHECKS = 0").then(function () {
+       return queryInterface.sequelize.query("truncate table codes", null, options);
+   }).then(function () {
+       return queryInterface.sequelize.query("SET FOREIGN_KEY_CHECKS = 1", null, options);
+   });
+
+   await queryInterface.bulkInsert("codes", codes, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('codes', null, {});
+  }
+};
---
+++ database/seeditems/codes-seed.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+module.exports = [
+    { group_id: 'A10', group_name: '클라이언트 타입' },
+    { group_id: 'A10', code_id: 'A10010', code_name: 'Web' },
+    { group_id: 'A10', code_id: 'A10020', code_name: 'iOS' },
+    { group_id: 'A10', code_id: 'A10030', code_name: 'Android' },
+    { group_id: 'A20', group_name: '사용자 상태' },
+    { group_id: 'A20', code_id: 'A20000', code_name: '인증대기' },
+    { group_id: 'A20', code_id: 'A20010', code_name: '인증완료' }
+]
